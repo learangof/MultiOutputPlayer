a96546d5f897d68ff4f2d00efe894e508521e9c7
Luis Arango <learangof@outlook.com>
2021-03-03 15:19:29 +1300

    add ts and gulp wacth fnc

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var ts = require("gulp-typescript");
+var refresh = require('gulp-refresh')
  
 sass.compiler = require('node-sass');
 
@@ -9,5 +11,27 @@ const root_output = './static/';
 gulp.task('sass', function () {
   return gulp.src(root_input + 'sass/base.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(root_output));
+    .pipe(gulp.dest(root_output))
+    .pipe(refresh());
 });
+
+gulp.task('ts', function () {
+    return gulp.src(root_input + 'ts/main.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            esModuleInterop: true,
+            target: "es6"
+        }))
+        .pipe(gulp.dest(root_output))
+        .pipe(refresh());
+});
+
+gulp.task('watch', function () {
+    refresh.listen();
+    gulp.watch(root_input + "sass/**/*.scss", gulp.parallel(['sass']));
+    gulp.watch(root_input + "ts/**/*.ts", gulp.parallel(['ts']));
+    gulp.watch("./views/**/*.html").on("change", refresh.reload);
+  });
+
+gulp.task('default', gulp.parallel(['sass', 'ts', 'watch', ]));
+gulp.task('prod', gulp.parallel(['sass', 'ts']));
