c182033d154c8c8b8d8d8643e86bf1099851e3ce
Luis Arango <learangof@outlook.com>
2021-03-11 14:59:53 +1300

    aad modal change sliders

--- src/static_/ts/slider.js
+++ src/static_/ts/slider.js
@@ -1,6 +1,6 @@
 import Slider from "./lib/Slider.js";
 export function init_slider() {
-    let slider2 = new Slider({
+    let slider1 = new Slider({
         Duration: 5585,
         backgroundColor: 'rgb(203, 255, 251)',
         barColor: 'blueviolet',
@@ -11,6 +11,18 @@ export function init_slider() {
         scale:'2',
         formatProgress: true,
       });
+      let slider2 = new Slider({
+        Duration: 5585,
+        backgroundColor: 'rgb(203, 255, 251)',
+        barColor: 'blueviolet',
+        height:'1.5rem',
+        hasBall: true,
+        hasIndicator: true,
+        ballRadius: '15px',
+        scale:'2',
+        formatProgress: true,
+      });
+    $('#slider1').append(slider1.getSlider());
     $('#slider2').append(slider2.getSlider());
 }
 
