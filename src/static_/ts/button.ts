a29a0abe1d594e39dd04b2ba1f44f57ae28eab54
Luis Arango <learangof@outlook.com>
2021-03-12 15:29:14 +1300

    add full screen btn and logic

--- src/static_/ts/button.ts
+++ src/static_/ts/button.ts
@@ -1,6 +1,7 @@
 export function init_buttons() {
   let childWindow:Window;
   let openButton:JQuery = $("#open-video");
+  let fullScreenButton:JQuery = $("#fullscreen-video");
   
   function openWindow() {   
     let windowFeatures:string = "new_instance=yes, toolbar=no, titlebar=no, scrollbars=no";
@@ -11,6 +12,7 @@ export function init_buttons() {
     openButton.off('click');
     openButton.click(closeWindow)
     toggleClassButtons(openButton);
+    fullScreenButton.prop("disabled", false);
 
     childWindow = window.open("video.html", "Video", windowFeatures);
     childWindow.onunload = closeWindowHandler;
@@ -28,7 +30,39 @@ export function init_buttons() {
     childWindow.close();
     toogleCloseWindow(); 
   }
-  
+  function fullScreenVideo() {
+    let video:any = childWindow.document.getElementById("video");
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    }else if (video.webkitRequestFullscreen) {/* Safari */
+      video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) {/* IE11 */
+      video.msRequestFullscreen();
+    }else if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen();
+    }
+    fullScreenButton.html("Close Full Screen");
+    fullScreenButton.off('click');
+    fullScreenButton.click(closeFullScreenVideo)
+    toggleClassButtons(fullScreenButton);
+
+  }
+  function closeFullScreenVideo() {
+    let video:any = childWindow.document.getElementById("video");
+    if (video.exitFullscreen) {
+      video.exitFullscreen();
+    }else if (video.webkitExitFullscreen) {/* Safari */
+      video.webkitExitFullscreen();
+    } else if (video.msExitFullscreen) {/* IE11 */
+      video.msExitFullscreen();
+    }else if (video.mozCancelFullScreen) {/* Mozilla */
+      video.mozCancelFullScreen();
+    }
+    fullScreenButton.html("Full Screen");
+    fullScreenButton.off('click');
+    fullScreenButton.click(fullScreenVideo)
+    toggleClassButtons(fullScreenButton);    
+  }
   function toggleClassButtons(button:JQuery) {
     button.toggleClass('is-info');
     button.toggleClass('is-danger');
@@ -38,7 +72,10 @@ export function init_buttons() {
     openButton.off('click');
     openButton.click(openWindow)
     toggleClassButtons(openButton);
+    fullScreenButton.prop("disabled", true); 
+    closeFullScreenVideo();
   }
 
   openButton.click(openWindow);
+  fullScreenButton.click(fullScreenVideo);
 }
