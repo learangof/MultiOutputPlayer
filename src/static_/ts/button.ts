4db566bd4e95bffa023b40dab396a70a9d80719b
Luis Arango <learangof@outlook.com>
2021-03-12 15:28:12 +1300

    improve open/close window logic & clean code

--- src/static_/ts/button.ts
+++ src/static_/ts/button.ts
@@ -1,134 +1,44 @@
 export function init_buttons() {
-  function openWindow() {
-    let childWindow:Window;
-    let button:HTMLButtonElement = this;
+  let childWindow:Window;
+  let openButton:JQuery = $("#open-video");
+  
+  function openWindow() {   
     let windowFeatures:string = "new_instance=yes, toolbar=no, titlebar=no, scrollbars=no";
     let isOpen:boolean = false;
 
-    button.disabled = true;
+    //openButton.prop("disabled", true);
+    openButton.html("Close Window");
+    openButton.off('click');
+    openButton.click(closeWindow)
+    toggleClassButtons(openButton);
 
     childWindow = window.open("video.html", "Video", windowFeatures);
-    childWindow.onunload = closeWindow;
-    childWindow.onbeforeunload = closeWindow;
+    childWindow.onunload = closeWindowHandler;
+    childWindow.onbeforeunload = closeWindowHandler;
     
-    function closeWindow(event:Event){
+    function closeWindowHandler(){
       //event.preventDefault();
-      (isOpen) ? button.disabled = false : isOpen = true;
+      if (isOpen) {
+        toogleCloseWindow();
+      }
+      isOpen = true;
     }
   }
-  $("#open-video").click(openWindow);
+  function closeWindow() {
+    childWindow.close();
+    toogleCloseWindow(); 
+  }
+  
+  function toggleClassButtons(button:JQuery) {
+    button.toggleClass('is-info');
+    button.toggleClass('is-danger');
+  }
+  function toogleCloseWindow() {
+    openButton.html("Open Window");
+    openButton.off('click');
+    openButton.click(openWindow)
+    toggleClassButtons(openButton);
+  }
+
+  openButton.click(openWindow);
 }
-
-// var childWindow;
-// var video;
-// var audio;
-// var devices;
-// var engAudio;
-// var videoAudio;
-// var config;
-// var language = {
-//     filePath: '',
-//     audioOutput: [],
-//     duration: 0,
-//     current: 0,
-//     start: 0,
-// }
-
-// if(!localStorage.getItem('config')){
-//     console.log("no data");
-//     config = {
-//         ESP: language,
-//         ENG: language
-//     }
-//     localStorage.setItem('config',JSON.stringify(config))
-// }else{
-//     console.log("data");
-//     config = JSON.parse(localStorage.getItem('config'));
-// }
-
-// console.log(config);
-
-// function openWindow() {
-//     this.disabled = true;
-//     document.querySelector("#play").disabled = false;
-//     var windowFeatures = "new_instance=yes, toolbar=no, titlebar=no, scrollbars=no";
-//     childWindow=window.open("video.html","Video",windowFeatures);
-// }
-
-// function fullSize() {
-//     // var elem = a.document.getElementById("vid");
-//     // console.log(a.document.fullscreenEnabled)
-//     //   if (elem.requestFullscreen) {
-//     //     elem.requestFullscreen();
-//     //   } else if (elem.webkitRequestFullscreen) { /* Safari */
-//     //     elem.webkitRequestFullscreen();
-//     //   } else if (elem.msRequestFullscreen) { /* IE11 */
-//     //     elem.msRequestFullscreen();
-//     //   }
-//     //a.resizeTo(screen.availWidth, screen.availHeight);
-// }
-
-// (async () => {
-//     await navigator.mediaDevices.getUserMedia({audio: true});
-//     devices = await navigator.mediaDevices.enumerateDevices();
-//     devices = devices.filter(device => (device.kind === 'audiooutput' && device.deviceId != "default" && device.deviceId != "communications"));
-//     var selectVideoAudio = document.querySelector('#video-audio');
-//     var selectEngAudio = document.querySelector('#eng-audio');
-//     for (var i = 0; i < devices.length; i++) {
-//         var option = document.createElement("option");
-//         option.textContent = devices[i].label;
-//         option.value = i;
-
-//         var opt = document.createElement("option");
-//         opt.textContent = devices[i].label;
-//         opt.value = i;
-
-//         selectVideoAudio.add(option)
-//         selectEngAudio.add(opt);
-//     }
-//     selectEngAudio.value = 1;
-// })();
-
-// function playMedia() {
-//     video = childWindow.document.querySelector("#vid");
-//     audio = document.querySelector("#aud");
-//     console.log(video.paused);
-//     if(videoAudio != engAudio){
-//         video.setSinkId(videoAudio);
-//         audio.setSinkId(engAudio);
-//         if(video.paused){
-//             audio.play();
-//             video.play();
-//             this.innerHTML = "pause";
-//         }else{
-//             audio.pause();
-//             video.pause();
-//             this.innerHTML = "play";
-//         }
-//     }else{
-//         alert("Same Audio Output, please Change it");
-//     }
-//     // video = childWindow.document.querySelector("#vid");
-//     // video.play();
-//     // console.log(video);
-//     this.di
-// }
-// function changeVideoAudio() {
-// // console.log(this.value," Label:", devices[this.value].label," ID:",devices[this.value].deviceId);
-//     videoAudio = devices[this.value].deviceId;
-//     config.ESP.audioOutput[0] = this.value
-//     config.ESP.audioOutput[1] = devices[this.value].label
-//     localStorage.setItem('config',JSON.stringify(config))
-// }
-// function changeEngAudio() {
-// // console.log(this.value," Label:", devices[this.value].label," ID:",devices[this.value].deviceId);
-//     engAudio = devices[this.value].deviceId;
-//     config.ENG.audioOutput[0] = this.value
-//     config.ENG.audioOutput[1] = devices[this.value].label
-//     localStorage.setItem('config',JSON.stringify(config))
-// }
-// document.querySelector('#open').addEventListener("click", openWindow);
-// document.querySelector('#full').addEventListener("click", fullSize);
-// document.querySelector('#play').addEventListener("click", playMedia);
-// document.querySelector('#video-audio').addEventListener("change", changeVideoAudio);
-// document.querySelector('#eng-audio').addEventListener("change", changeEngAudio);
