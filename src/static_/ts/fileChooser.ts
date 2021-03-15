c91d9b6dc9d8763ecdc5affc771e271882cc34ac
Luis Arango <learangof@outlook.com>
2021-03-15 13:35:32 +1300

    add file accept and add audio id

--- src/static_/ts/fileChooser.ts
+++ src/static_/ts/fileChooser.ts
@@ -1,12 +1,26 @@
 import { getChildWindow, ableOpenButton } from "./button.js";
 
+let video:File;
+let audio:File;
 export function init_filesChoosers() {
-  const fileInput: HTMLInputElement = document.querySelector("#video input[type=file]");
-  fileInput.onchange = () => {    
-    if (fileInput.files.length > 0) {
-      ableOpenButton();
-      const fileName: HTMLElement = document.querySelector("#video .file-name");
-      fileName.textContent = fileInput.files[0].name;
+  const fileVideoInput: JQuery = $("#video input[type=file]");
+  const fileAudioInput: JQuery = $("#audio input[type=file]");
+  fileVideoInput.on("change",{ isVideo: true },changeName);
+  fileAudioInput.on("change",{ isVideo: false },changeName);
+
+  function changeName(isVideo) {
+    console.log(this);
+    if (this.files.length > 0) {
+      const fileName: JQuery = $(this).siblings('.file-name');
+      console.log(fileName);
+      fileName.html(this.files[0].name);
+      (isVideo)? ableOpenButton() : "";
+      (isVideo)? video = this.files[0] : audio = this.files[0];
     }
-  };
+  }
+}
+export function getFile(type:string) {
+  let file:File;
+  (type == 'video')? file = video : file = audio;
+  return file; 
 }
