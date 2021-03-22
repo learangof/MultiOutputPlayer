2525725aaa104a1ee349e9b27a4f8b2c2c89db7f
Luis Arango <learangof@outlook.com>
2021-03-23 11:55:31 +1300

    fix input behavi

--- src/static_/ts/player.ts
+++ src/static_/ts/player.ts
@@ -1,4 +1,5 @@
 import { setCurrentTime } from "./slider.js"
+let startAt:JSON = JSON.parse('{}');
 export function initPlayer() {
     initMainControls();
     initVideoControls();
@@ -13,10 +14,11 @@ function initVideoControls() {
 function initAudioControls() {
     let name = '#audio';
     $('button[data-for="'+name+'"').each(function (index){
+        startAt[name] = 0;
         $(this).click(function (event:Event) {
             EVENTS[this.id.split('-')[0]](this, name, <HTMLMediaElement>$(name)[0]);
         });
-    });
+    });    
 }
 const EVENTS = {
     play(btn:HTMLButtonElement, playerId:string, player:HTMLMediaElement) {
@@ -55,23 +57,37 @@ const EVENTS = {
         setCurrentTime(player.currentTime, playerId);
     }
 };
-$(".input[data-for='#audio']").on('keydown', function (event:any) {
-    var selection = window.getSelection().toString();
+$(".input[data-for='#audio']").on('keyup', function (event:any) {
+
+    let selection:string = window.getSelection().toString();
     if (selection !== '') {
         return;
     }
+
     if ($.inArray(event.keyCode, [38, 40, 37, 39, 46, 8]) !== -1) {
         return;
     }
-    var $this = $(this);
-    var input = $this.val();
-    if (input.match(/[^:\d]/) || this.value.length > 7) {
+
+    if(this.value.length > 8) {
+        this.value = this.value.slice(0,8);
+        return;
+    }
+
+    let $this:JQuery = $(this);
+    let input:string = <string>$this.val();
+    if (input.match(/[^:\d]/)) {
         this.value = this.value.slice(0, -1);
         return;
     }
+    input = <string>$this.val();
+    
     //TODO autoformaring to fufill always 00:00:00 format
+    let time:string[] = input.split(':');
+    
     input = input.replace(/[\D\s\._\-]+/g, "");
     input = input.replace(/\d{2}/g, "$&:");
+    (input.length > 8 )? input = input.slice(0,8) : '';
+    
     $this.val(function () {        
         return input;
     });
