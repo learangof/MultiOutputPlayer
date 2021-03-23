efeba72be4c7923f22c2238e727b52d3cc8586c8
Luis Arango <learangof@outlook.com>
2021-03-24 10:54:55 +1300

    generalize functon

--- src/static_/ts/player.ts
+++ src/static_/ts/player.ts
@@ -17,6 +17,9 @@ function initAudioControls() {
         $(this).click(function (event:Event) {
             EVENTS[this.id.split('-')[0]](this, name, <HTMLMediaElement>$(name)[0]);
         });
+    });
+    $(".input[data-for='"+name+"']").on('keyup',function(event:any){
+        EVENTS[this.id](this, event)
     });    
 }
 const EVENTS = {
@@ -70,55 +73,58 @@ const EVENTS = {
         let forTime:number = parseInt(btn.id.split('-')[1]);
         player.currentTime -= forTime;        
         setCurrentTime(player.currentTime, playerId);
+    },
+    startTime(input:HTMLInputElement, event:any) {
+        let selection: string = window.getSelection().toString();
+        if (selection !== '') {
+            return;
+        }
+
+        if ($.inArray(event.keyCode, [38, 40, 37, 39, 46, 8]) !== -1) {
+            return;
+        }
+
+        if (input.value.length > 8) {
+            input.value = input.value.slice(0, 8);
+            return;
+        }
+
+        let $input: JQuery = $(input);
+        let value: string = <string>$input.val();
+        if (value.match(/[^:\d]/)) {
+            input.value = input.value.slice(0, -1);
+            return;
+        }
+        value = <string>$input.val();
+        //TODO autoformaring to fufill always 00:00:00 format
+        value = value.replace(/[\D\s\._\-]+/g, "");
+        // let inputArray:string[] = Array.from(input);
+        // input = "";
+        // for (let index = 0; index < 6; index++) {
+        //     (inputArray[index])? input += inputArray[index] : input += "0"; 
+        // }
+        // console.log(input);
+        value = value.replace(/\d{2}/g, "$&:");
+        ((value.length == 3 && value.slice(-1) == ":") || (value.length == 6 && value.slice(-1) == ":")) ? value = value.slice(0, -1) : '';
+        (value.length > 8) ? value = value.slice(0, 8) : '';
+        //TODO Check numbers is limit to 59 to avoi 80seconds or 90minutes so on
+        // let inputArray:string[] = input.split(':');
+        // input = ""
+        // inputArray.forEach(function(ele){
+        //     let num:number = parseInt(ele);
+        //     (num > 59)? num = 59 : '';
+        //     input += num;
+        // });
+        // input = input.replace(/\d{2}/g, "$&:");
+        startAt[input.dataset.for] = value.split(':');
+
+        $input.val(function () {
+            return value;
+        });
     }
 };
 
 $(".input[data-for='#audio']").on('keyup', function (event:any) {
 
-    let selection:string = window.getSelection().toString();
-    if (selection !== '') {
-        return;
-    }
-
-    if ($.inArray(event.keyCode, [38, 40, 37, 39, 46, 8]) !== -1) {
-        return;
-    }
-
-    if(this.value.length > 8) {
-        this.value = this.value.slice(0,8);
-        return;
-    }
-
-    let $this:JQuery = $(this);
-    let input:string = <string>$this.val();
-    if (input.match(/[^:\d]/)) {
-        this.value = this.value.slice(0, -1);
-        return;
-    }
-    input = <string>$this.val();
-    //TODO autoformaring to fufill always 00:00:00 format
-    input = input.replace(/[\D\s\._\-]+/g, "");
-    // let inputArray:string[] = Array.from(input);
-    // input = "";
-    // for (let index = 0; index < 6; index++) {
-    //     (inputArray[index])? input += inputArray[index] : input += "0"; 
-    // }
-    // console.log(input);
-    input = input.replace(/\d{2}/g, "$&:");
-    ((input.length == 3 && input.slice(-1) == ":") || (input.length == 6 && input.slice(-1) == ":"))? input = input.slice(0,-1) : '';
-    (input.length > 8)? input = input.slice(0,8) : '';
-    //TODO Check numbers is limit to 59 to avoi 80seconds or 90minutes so on
-    // let inputArray:string[] = input.split(':');
-    // input = ""
-    // inputArray.forEach(function(ele){
-    //     let num:number = parseInt(ele);
-    //     (num > 59)? num = 59 : '';
-    //     input += num;
-    // });
-    // input = input.replace(/\d{2}/g, "$&:");
-    startAt[this.dataset.for] = input.split(':');
     
-    $this.val(function () {        
-        return input;
-    });
 });
