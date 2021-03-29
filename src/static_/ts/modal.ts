a6429635697baf623dccf2c9c4d15e46e1f5079a
Luis Arango <learangof@outlook.com>
2021-03-30 09:40:06 +1300

    validate outputs

--- src/static_/ts/modal.ts
+++ src/static_/ts/modal.ts
@@ -1,12 +1,30 @@
+import { selectedOutputs } from './audio.js';
+
 export function init_modal() {
-    function toggleModalClasses(event:Event) {
-        let target:HTMLElement = <HTMLElement>event.currentTarget;
-        let modalId:String = target.dataset.modalId;
-        let modal:JQuery = $(modalId);
-        modal.toggleClass('is-active');
-        $('html').toggleClass('is-clipped');
-    }
     $('.open-config').click(toggleModalClasses);
-    $('.close-config').click(toggleModalClasses); 
+    $('.close-config').click(checkAudioOutputs); 
+}
+function toggleModalClasses(event:Event) {
+    let target:HTMLElement = <HTMLElement>event.currentTarget;
+    let modalId:String = target.dataset.modalId;
+    let modal:JQuery = $(modalId);
+    modal.toggleClass('is-active');
+    $('html').toggleClass('is-clipped');
+}
+function checkAudioOutputs(event:Event) {
+    let tempItem:string = '';
+    let sameOutputs:boolean = false;
+    for (const item in selectedOutputs) {
+        if (tempItem == selectedOutputs[item][0]) {
+            sameOutputs = true;
+        }
+        tempItem = selectedOutputs[item][0]
+    }
+    if (!sameOutputs) {
+        toggleModalClasses(event);
+    }else {
+        alert ("audio outputs are the same");
+    }
+    
 }
 
